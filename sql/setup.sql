eb8f20193b5e48d4fddb1ab799a48118c9a0cc45
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-06-29 17:24:02 -0400

    Add basic tables for posts

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,1 +1,24 @@
-CREATE TABLE posts 
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(200),
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE attributes (
+    id SERIAL PRIMARY KEY,
+    post_id INT REFERENCES posts(id),
+    title VARCHAR(200),
+    content TEXT DEFAULT '',
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (post_id) REFERENCES posts(id),
+);
+
+CREATE TABLE threads (
+    id SERIAL PRIMARY KEY,
+    attribute_id INT REFERENCES atttributes(id),
+    content TEXT DEFAULT '',
+    user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+);
