13d69961d293a32fc1a92744a7c36478f105e10f
Asher Spurr <144856487+AsherSpurr@users.noreply.github.com>
2024-06-29 20:14:50 -0400

    Install express pg

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,6 @@
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(200),
+    category VARCHAR(200),
     created_at TIMESTAMP DEFAULT NOW()
 );
 
@@ -11,6 +11,7 @@ CREATE TABLE attributes (
     content TEXT DEFAULT '',
     user_id INT NOT NULL,
     created_at TIMESTAMP DEFAULT NOW(),
+    threads JSONB DEFAULT [],
     FOREIGN KEY (post_id) REFERENCES posts(id),
 );
 
@@ -22,3 +23,11 @@ CREATE TABLE threads (
     created_at TIMESTAMP DEFAULT NOW(),
     FOREIGN KEY (user_id) REFERENCES users(id),
 );
+
+
+INSERT INTO posts (category) VALUES 
+('Cleaning'),
+('Treatment'),
+('General');
+
+INSERT INTO attributes (post_id, title, content, user_id) VALUES (1, 'title', 1);
