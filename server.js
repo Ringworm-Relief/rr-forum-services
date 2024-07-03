97f2c272815663e38cec39ced1cb1a2634ce905e
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2024-07-02 19:49:59 -0700

    Update Pool

--- server.js
+++ server.js
@@ -8,15 +8,26 @@ const port = process.env.PORT || 3000;
 require('dotenv').config();
 
 // Configure the connection db_session to PostgreSQL using environment variables
+// const db_session = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+// });
+
 const db_session = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    }
 });
 
 app.use(express.json());
+
+db_session.connect()
+  .then(() => console.log('Connected to PostgreSQL database'))
+  .catch(err => console.error('Connection error', err.stack));
 
 const corsOptions = {
   origin: [/^http:\/\/localhost:\d+$/, "https://rr-as.vercel.app/"],
